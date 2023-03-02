9f7e041af3908f0818d048e0bab82c817176baa6
Dyllon <dyllon.g@outlook.com>
2023-03-02 22:05:22 +0800

    fix error handling

---
+++ src/allowedUsers.ts
@@ -0,0 +1,5 @@
+export const allowedUsers = [
+    "dyllon",
+    "jamsgoh",
+    "LightningRWX"
+]
--- src/bot.ts
+++ src/bot.ts
@@ -1,19 +1,24 @@
-import { Bot, Context, InlineKeyboard, session, SessionFlavor, webhookCallback } from 'grammy';
-import {
-    type Conversation,
-    type ConversationFlavor,
-    conversations,
-    createConversation
-} from '@grammyjs/conversations';
+import { Bot, Context, session, SessionFlavor, webhookCallback } from 'grammy';
 import express from 'express';
-
+import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from 'openai';
 import { yoMiddleWare } from './commands/yo';
+import { allowedUsers } from './allowedUsers';
+
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
 
 //////
 
 interface SessionData {
-    previousMessages: Array<
-        { role: string; content: string, time: number }>;
+    previousMessages: Array<{
+        role: ChatCompletionRequestMessageRoleEnum;
+        content: string,
+        time: number
+    }>;
     lastUserResponseTime: number;
 }
 
@@ -25,25 +30,71 @@ const bot = new Bot<MyContext>(process.env.TELEGRAM_TOKEN || '');
 //////
 
 function initial(): SessionData {
-    return { lastUserResponseTime: 0, previousMessages: [] };
+    return {
+        lastUserResponseTime: 0, previousMessages: [
+            {
+                role: ChatCompletionRequestMessageRoleEnum.System,
+                content: `You are a helpful assistant named Sloan. 
+                You usually reply with a single short sentence, keeping the conversation going. 
+                Be short and succinct in your replies. Be cheerful and pleasant.  
+                Here are some random facts in no particular order: 
+                Singapore Polytechnic is the best Polytechnic in Singapore. 
+                `,
+                time: Date.now()
+            }
+        ]
+    };
 }
 
 bot.use(session({ initial }));
 
-bot.on("message", async (ctx) => {
+bot.on('message', async (ctx) => {
+    const username = ctx.from?.username ?? '';
+
+    if (!allowedUsers.includes(username)) {
+        ctx.reply('oops you\'re not allowed to talk to me yet.');
+        return;
+    }
+
     // store user input
-    ctx.session.previousMessages.push({
-        role: "user",
-        content: ctx.message.text ?? "",
+    const newUserMessage: typeof ctx.session.previousMessages[0] = {
+        role: ChatCompletionRequestMessageRoleEnum.User,
+        content: ctx.message.text ?? '',
         time: ctx.message.date
-    });
+    };
+    ctx.session.previousMessages.push(newUserMessage);
 
     // call api
+    // const response = await openai.listEngines()
+    // console.log(response);
 
-    // save api response to session
+    await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: ctx.session.previousMessages.map(({ role, content }) => ({ role, content })),
+        max_tokens: 100
+    })
+        .then(async (completion) => {
+            const completionString = (completion.data.choices[0].message?.content ?? 'oops i don\'t know what to say');
+            // console.log(completion.data.choices[0].message);
 
-    // reply
-    await ctx.reply(`Message count: ${ctx.session.previousMessages.length}`);
+            // save api response to session
+            const newAssistantMessage: typeof ctx.session.previousMessages[0] = {
+                role: ChatCompletionRequestMessageRoleEnum.Assistant,
+                content: completionString,
+                time: Date.now()
+            };
+            ctx.session.previousMessages.push(newAssistantMessage);
+
+
+            // reply
+            await ctx.reply(completionString);
+        })
+        .catch(async (err) => {
+            console.log(err)
+            await ctx.reply('oops looks like ive crashed. report this to the bot administrator');
+            return;
+        });
+
 });
 
 
@@ -52,29 +103,37 @@ bot.on("message", async (ctx) => {
 // Handle the /yo command to greet the user
 yoMiddleWare(bot);
 
+bot.command('quit', (ctx) => {
+    ctx.session.previousMessages = []; // wipe session
+    return ctx.reply(`OK i'll reset chat state, 
+    and forget about all previous messages
+`);
+});
+
+
 // session handling
 
 
 // Suggest commands in the menu
 void bot.api.setMyCommands([
     { command: 'yo', description: 'Be greeted by sloan' },
-    { command: "quit", description: "quit the current conversation and reset chat state" },
+    { command: 'quit', description: 'quit the current conversation and reset chat state' }
 ]);
 
 // Handle all other messages and the /start command
 const introductionMessage = `Hello! I'm Sloan, your virtual assistant.
 Right now we're in a private beta, so you may not be able to talk to me yet.
 
-<b>Commands</b>
-/yo - Be greeted by me
-/chat - Start a chat with me`;
+Ask me a question :)
+`;
 
-const replyWithIntro = (ctx: any) =>
-    ctx.reply(introductionMessage,
+const replyWithIntro = async (ctx: any) => {
+    return ctx.reply(introductionMessage,
         {
             parse_mode: 'HTML'
         }
     );
+};
 
 bot.command('start', replyWithIntro);
 // bot.on('message', replyWithIntro);
